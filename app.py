75ad3ee88a97c88b5d41ea541e37105dcd5b8529
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-10-25 20:08:42 -0500

    continued work on flask routes

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 
 file_path = os.path.join("Resources", "Temp_Average_Auto_Insurance_Data.csv")
 DATA = pd.read_csv(file_path)
-print(DATA['Zip_Code'].dtype)
+#print(DATA['Zip_Code'].dtype) <-- this was int64
 
 #################################################
 # Flask Routes
@@ -53,138 +53,89 @@ def welcome():
 def zip(by_Zip = None):
     """Return the car insurance and weather data by zip code"""
     try:
-        new_df = DATA.loc[DATA["Zip_Code"] == int(by_Zip)]
-        new_dict = new_df.to_dict(orient="records")
-        return jsonify(new_dict)
-    except Exception as e:
-        print(e)
-        return jsonify([])
+        working_zip = DATA.loc[DATA["Zip_Code"] == int(by_Zip)]
+        zip_dict = working_zip.to_dict(orient="records")
+        return jsonify(zip_dict)
+    except Exception as error:
+        print(error)
+        return jsonify([f'WRONG'])
+        #return jsonify([])
 
 ### some notes from AskBCS:
 ##  results = df.loc[df['zip'] == search_zip]
 
-
-
-
-
-
-
-    # search_zip = by_Zip
-    # zip_search_list = []
-
-    # for zip in test:
-    #     zipcode_result = test['by_Zip...or whatever we call the zip column']
-    #         #.replace(" ","").lower()
-    #     zip_test_dict = {}
-
-    #     if zipcode_result == search_zip:
-    #         #return jsonify(zip)
-
-    #         zip_test_dict['min temp']= zip.min
-    #         zip_test_dict['max temp']= zip.max
-    #         zip_test_dict['average temp']= zip.avg
-    #         zip_search_list.append(zip_test_dict)
-
-    #         return jsonify(zip_search_list)
-
     # return jsonify({'error':"Something went horribly wrong, no one ever learned to drive a car in your area."})
 
-
-
-
-    #################
-        # if end:
-
-        # init_query = session.query(func.min(measurement.tobs).label('min'), func.max(measurement.tobs).label('max'), func.avg(measurement.tobs).label('avg'))
-        # results_end = init_query.filter(measurement.date >= start).filter(measurement.date <= end).all()
-
-        # else:
-
-        # init_query = session.query(func.min(measurement.tobs).label('min'), func.max(measurement.tobs).label('max'), func.avg(measurement.tobs).label('avg'))
-        # results_end = init_query.filter(measurement.date >= start).all()
-
-        # return jsonify(hawaii_measurements_dates) 
-    #################
-
-
-
-### if we wanted to combine all available data within the flask app... we could utilize he Hawaii example..
-    # hawaii_measurements_dates = []
-    # for row in results_end:
-    #     measurements = {}
-    # # here, utlizing my orig earlier idea from the Titanic example
-    #     measurements['min temp']= row.min
-    #     measurements['max temp']= row.max
-    #     measurements['average temp']= row.avg
-    #     hawaii_measurements_dates.append(measurements) 
-
-    # return jsonify(hawaii_measurements_dates)   
-
-
+  
 
 # return the data according to city entry
-@app.route("/api/v1.0/<by_City>")
+@app.route("/api/v1.0/city/<by_City>")
 def city(by_City = None):
     """Return the car insurance and weather data by Zip Code"""
-
-    file_path = os.path.join("Resources", "Temp_Average_Auto_Insurance_Data.csv")
-    data = pd.read_csv(file_path)
-
-    # with open("..", "Resources", "Temp_Average_Auto_Insurance_Data.csv") as csv_file:
-    #     data = csv.reader(csv_file, delimiter=',')
-    #     #first_line = True
-    search_city = by_City
-    city_list = []
-    for row in data:
-        if data['City'] == search_city:
-            city_list.append({
-            "State": row[0],
-            "Zip_Code": row[1],
-            "City": row[2],
-            "Avg Rate":row[3]
-            })
-            print(city_list)
-        else:
-            print('No match for this City')
-        #return render_template("index.html", places=places)
-    return zip_list
-    # search_city = by_City
-
-    # for city in test:
-    #     city_result = test['by_City']
-    #         #.replace(" ","").lower()
+    
+    ## Need some method in here for .lower example in justice league 
+            # for city in test:
+    #     city_result = test['by_City'].replace(" ","").lower()
 
     #     if city_result == search_city:
     #         return jsonify(test)
 
+    try:
+        working_zip2 = DATA.loc[DATA["City"] == by_City]
+        city_dict = working_zip2.to_dict(orient="records")
+        return jsonify(city_dict)
+    except Exception as error:
+        print(error)
+        return jsonify([])
+
     # return jsonify({'error':"Something went horribly wrong; Tesla overtook your city - autopilot only = no insurance necessary."})
 
-# return the data according to city entry
-@app.route("/api/v1.0/<by_State>")
+
+# return the data according to state entry
+@app.route("/api/v1.0/state/<by_State>")
 def state(by_State = None):
     """Return the car insurance and weather data by State"""
 
-    search_State = by_State
+    ## Need some method in here for .lower example in justice league 
+            # for state in test:
+    #     city_result = test['by_City'].replace(" ","").lower()
 
-    for state in test:
-        state_result = test['by_State']
-            #.replace(" ","").lower()
+    #     if city_result == search_city:
+    #         return jsonify(test)
+    
+    try:
+        working_zip3 = DATA.loc[DATA["State"] == by_State]
+        state_dict = working_zip3.to_dict(orient="records")
+        return jsonify(state_dict)
+    except Exception as error:
+        print(error)
+        return jsonify([])
 
-        if state_result == search_state:
-            return jsonify(test)
-
-    return jsonify({'error':"Something went horribly wrong; your state has been taken over by murder hornets - no driving for you!"})
+    # return jsonify({'error':"Something went horribly wrong; your state has been taken over by murder hornets - no driving for you!"})
 
 # return the data according to city entry
 @app.route("/api/v1.0/all_data")
 def all():
     """Return all the car insurance and weather data"""
 
-    file_path = os.path.join("Resources", "Temp_Average_Auto_Insurance_Data.csv")
-    data = pd.read_csv(file_path)
+## Don't know how we want to display this ...
 
-    return jsonify(data)
 
+
+    return(
+    DATA.head()
+    )
+
+
+    # try:
+    #     working_zip4 = DATA.loc[DATA[""] == by_State]
+    #     state_dict = working_zip3.to_dict(orient="records")
+    #     return jsonify(state_dict)
+    # except Exception as error:
+    #     print(error)
+    #     return jsonify([])
+
+    # return jsonify(DATA)
 
 
 if __name__ == "__main__":
