0de8eb8bf15fc58df5fd8b7424afb5245062c46b
Emeka Obianyor <obianyore@gmail.com>
2020-10-27 18:52:56 -0500

    Worked on a postgres batabase and a jupyter notebook for postgres connection and table join for flask app

--- Build_2_Tables.sql
+++ Build_2_Tables.sql
@@ -4,27 +4,30 @@
 -- -- ---------------------------------------------------------------------------------------------------------------------
 -- -- IF YOU MAKE A MISTAKE AND NEED TO START OVER, YOU'D NEED TO DROP THE TABLES AND RECREATE THEM
 
--- DROP TABLE "Auto_Insurance_Data";
--- DROP TABLE "Weather_Data";
+-- DROP TABLE Auto_Insurance_Data;
+-- DROP TABLE Weather_Data;
 
 -- -- ---------------------------------------------------------------------------------------------------------------------
 -- -- CREATE TABLES
 
 CREATE TABLE Weather_Data (
+	ID SERIAL,
     Zip_Code Integer   NOT NULL,
-    City Varchar   NOT NULL,
     Weather_Forecast Varchar   NOT NULL,
     Weather_Description Varchar   NOT NULL,
     Max_Temperature Float   NOT NULL,
     Min_Temperature Float   NOT NULL,
-    Humidity Float   NOT NULL
+    Humidity Float   NOT NULL,
+	PRIMARY KEY (ID)
 );
 
 CREATE TABLE Auto_Insurance_Data (
+	ID SERIAL,
     State Varchar   NOT NULL,
     Zip_Code Integer   NOT NULL,
     City Varchar   NOT NULL,
-    Average_Auto_Insurance_Rate Float   NOT NULL
+    Average_Auto_Insurance_Rate Float   NOT NULL,
+	PRIMARY KEY (ID)
 );
 
 -- -- ---------------------------------------------------------------------------------------------------------------------
@@ -34,7 +37,7 @@ CREATE TABLE Auto_Insurance_Data (
 -- -- ---------------------------------------------------------------------------------------------------------------------
 -- -- CREATE JOINED TABLE
 
--- SELECT a.State, a.Zip_Code, w.Zip_Code, a.city, w.city, w.Weather_Forecast, w.Weather_Description, w.Max_Temperature,
+-- SELECT a.State, a.Zip_Code, w.Zip_Code, a.city, w.Weather_Forecast, w.Weather_Description, w.Max_Temperature,
 -- 	   w.Min_Temperature, w.Humidity
 -- FROM Auto_Insurance_Data AS a
 -- INNER JOIN Weather_Data AS w 
