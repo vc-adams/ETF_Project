77bd38065d9af10158c89810c06e370637f0bbcc
Emeka Obianyor <obianyore@gmail.com>
2020-10-27 20:56:34 -0500

    To prevent conflict

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,9 @@
+# Add your weather API key
+api_key = "7e98628ecd4395a6ca3f342c5abe7ccc"
+
+# SQL Server Info
+dbuser = 'postgres'
+dbpassword = 'Sm6Jc5bqbiNQdsVAo7eN'
+dbhost = 'localhost'
+dbport = '5432'
+dbname= 'ETL-Project1'
